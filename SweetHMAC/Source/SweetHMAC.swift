968d2573d2ae7934e5a5fbe86a66ab377a166551
Jan Cassio <hey@jancassio.com>
2016-04-14 20:28:46 -0300

    added static methods and more string extensions, to generate hashes without pass a secret key

--- SweetHMAC/Source/SweetHMAC.swift
+++ SweetHMAC/Source/SweetHMAC.swift
@@ -24,9 +24,9 @@
 
 
 /*
-Special thanks to jernejstrasner to provide the gist that inspired this approach
-URL: https://gist.github.com/jernejstrasner/1d5fa5e2fabda2e729d1
-*/
+ Special thanks to jernejstrasner to provide the gist that inspired this approach
+ URL: https://gist.github.com/jernejstrasner/1d5fa5e2fabda2e729d1
+ */
 
 import Foundation
 import CommonCrypto
@@ -34,23 +34,47 @@ import CommonCrypto
 public extension String {
   
   /**
-  Proved a string for some HMAC algorithm and secret string.
-  
-  - parameter algorithm: Some HMAC algorithm. Supported types are:
-  
-  - MD5
-  - SHA1
-  - SHA224
-  - SHA256
-  - SHA348
-  - SHA512
-  
-  - parameter secret: A secret message to authenticate the encrypted message.
-  
-  - returns: A encryped string based on HMAC algorithm and secret string.
-  */
+   Proved a string for some HMAC algorithm and secret string.
+   
+   - parameter algorithm: Some HMAC algorithm. Supported types are:
+   
+   - MD5
+   - SHA1
+   - SHA224
+   - SHA256
+   - SHA384
+   - SHA512
+   
+   - parameter secret: A secret message to authenticate the encrypted message.
+   
+   - returns: A encryped string based on HMAC algorithm and secret string.
+   */
   func HMAC(algorithm:HMACAlgorithm, secret:String) -> String {
     return SweetHMAC(message: self, secret: secret).HMAC(algorithm)
+  }
+  
+  func MD5 () -> String {
+    return SweetHMAC.MD5(self)
+  }
+  
+  func SHA1 () -> String {
+    return SweetHMAC.SHA1(self)
+  }
+  
+  func SHA224 () -> String {
+    return SweetHMAC.SHA224(self)
+  }
+  
+  func SHA256 () -> String {
+    return SweetHMAC.SHA256(self)
+  }
+  
+  func SHA348 () -> String {
+    return SweetHMAC.SHA384(self)
+  }
+  
+  func SHA512 () -> String {
+    return SweetHMAC.SHA512(self)
   }
 }
 
@@ -58,24 +82,24 @@ public extension String {
 
 
 /**
-HMACAlgoriths enumerates all HMAC algorithms types supported by iOS (not verified in Mac OS X environment yet)
-Supported iOS HMAC algorithms:
-
-- MD5
-- SHA1
-- SHA224
-- SHA256
-- SHA348
-- SHA512
-*/
+ HMACAlgoriths enumerates all HMAC algorithms types supported by iOS (not verified in Mac OS X environment yet)
+ Supported iOS HMAC algorithms:
+ 
+ - MD5
+ - SHA1
+ - SHA224
+ - SHA256
+ - SHA348
+ - SHA512
+ */
 public enum HMACAlgorithm {
   case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
   
   /**
-  Give the native value for HMACAlgorithm value
-  
-  - returns: The system `CCHmacAlgorithm` native value.
-  */
+   Give the native value for HMACAlgorithm value
+   
+   - returns: The system `CCHmacAlgorithm` native value.
+   */
   func toNative () -> CCHmacAlgorithm {
     switch self {
     case .MD5:
@@ -139,26 +163,26 @@ public class SweetHMAC {
   
   
   /// Message to be encrypted
-  private var message:String!
+  private var message:String = ""
   
   /// Secret message to authenticate the encrypted message.
-  private var secret:String!
+  private var secret:String = ""
   
   /**
-  Create a new SweetHMAC instance with given message and secret strings.
-  - parameter message: The message to be encrypted.
-  - parameter secret: The secret message to authenticate encrypted message.
-  */
+   Create a new SweetHMAC instance with given message and secret strings.
+   - parameter message: The message to be encrypted.
+   - parameter secret: The secret message to authenticate encrypted message.
+   */
   public init(message:String, secret:String) {
     self.message = message
     self.secret = secret
   }
   
   /**
-  Generate HMAC string with given algorithm.
-  - parameter algorithm: Algorithm to encrypt message.
-  - returns: A encrypted string.
-  */
+   Generate HMAC string with given algorithm.
+   - parameter algorithm: Algorithm to encrypt message.
+   - returns: A encrypted string.
+   */
   public func HMAC(algorithm:HMACAlgorithm) -> String {
     let seed  = UTF8EncodedString(string: message)
     let key   = UTF8EncodedString(string: secret)
@@ -167,6 +191,110 @@ public class SweetHMAC {
     let result = UnsafeMutablePointer<CUnsignedChar>.alloc( digestLength )
     
     CCHmac(algorithm.toNative(), key.data, key.length, seed.data, seed.length, result)
+    
+    let hash = NSMutableString()
+    
+    for i in 0..<digestLength {
+      hash.appendFormat("%02x", result[i])
+    }
+    
+    result.destroy()
+    
+    return String( hash )
+  }
+  
+  /**
+   Generate MD5 hash from the input string provided.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  public class func MD5 (input: String) -> String {
+    return digest(.MD5, input: input)
+  }
+  
+  /**
+   Generate SHA1 hash from the input string provided.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  public class func SHA1 (input: String) -> String {
+    return digest(.SHA1, input: input)
+  }
+  
+  /**
+   Generate SHA224 hash from the input string provided.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  public class func SHA224 (input: String) -> String {
+    return digest(.SHA224, input: input)
+  }
+  
+  /**
+   Generate SHA256 hash from the input string provided.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  public class func SHA256 (input: String) -> String {
+    return digest(.SHA256, input: input)
+  }
+  
+  /**
+   Generate SHA384 hash from the input string provided.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  public class func SHA384 (input: String) -> String {
+    return digest(.SHA384, input: input)
+  }
+  
+  /**
+   Generate SHA512 hash from the input string provided.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  public class func SHA512 (input: String) -> String {
+    return digest(.SHA512, input: input)
+  }
+  
+  
+  /**
+   Generate abstraction for static methods
+   - parameter algorithm: Algorithm to encrypt message.
+   - parameter input: The string to be encrypted.
+   - returns: A encrypted string.
+   */
+  private class func digest (algorithm: HMACAlgorithm, input: String) -> String {
+    let seed  = UTF8EncodedString(string: input)
+    let digestLength = algorithm.digestLength()
+    
+    let result = UnsafeMutablePointer<UInt8>.alloc( digestLength )
+    
+    switch algorithm {
+    case .MD5:
+      CC_MD5(seed.data, CC_LONG(seed.length), result)
+      break
+      
+    case .SHA1:
+      CC_SHA1(seed.data, CC_LONG(seed.length), result)
+      break
+      
+    case .SHA224:
+      CC_SHA224(seed.data, CC_LONG(seed.length), result)
+      break
+      
+    case .SHA256:
+      CC_SHA256(seed.data, CC_LONG(seed.length), result)
+      break
+      
+    case .SHA384:
+      CC_SHA384(seed.data, CC_LONG(seed.length), result)
+      break
+      
+    case .SHA512:
+      CC_SHA512(seed.data, CC_LONG(seed.length), result)
+      break
+    }
     
     let hash = NSMutableString()
     
